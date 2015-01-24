34d963a4023f00fb3f470c08076a30dae28f3c02
Paco Hobi <pacohobi@me.com>
2015-01-24 01:34:37 -0500

    Changed the channel name in hte config dictionary to lowercase to precent cases problems

--- firebot.py
+++ firebot.py
@@ -153,10 +153,12 @@ giveaway = Giveaway.Giveaway(conn, config)
 users = {} # users dictionary
 
 if len(sys.argv) > 1: # if channel specified as an argument we ignore the channel in config
-	config['channel'] = sys.argv[1]
+	config['channel'] = sys.argv[1].lower()
+else:
+	config['channel'] = config['channel'].lower()
 conn.connect('irc.twitch.tv') # connect to the server
 conn.login(config['bot_user'], config['bot_password']) # login the bot
-conn.join('#'+config['channel'].lower()) # join the channel
+conn.join('#'+config['channel']) # join the channel
 conn.send("TWITCHCLIENT 3") # to receive user info (usermode, color, emotesets) and twitchnotify (user subscriptions)
 #open file for log chat
 if (config['log_chat']):
